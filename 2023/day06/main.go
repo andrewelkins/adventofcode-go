b37dc03eb7e63a257ea9d031449bdaff5868a3d0
Andrew Elkins <andrewelkins@users.noreply.github.com>
2023-12-06 12:54:02 -0800

    Move part2 to quad

--- 2023/day06/main.go
+++ 2023/day06/main.go
@@ -69,8 +69,8 @@ func part2(input string) int {
 	time := cast.ToFloat(strings.ReplaceAll(strings.Split(parsed[0], ":")[1], " ", ""))
 	distance := cast.ToFloat(strings.ReplaceAll(strings.Split(parsed[1], ":")[1], " ", "")) + 1
 
-	low := math.Floor((time + math.Sqrt(math.Pow(time, 2) - 4 * distance))/2)
-	high := math.Ceil((time - math.Sqrt(math.Pow(time, 2) - 4 * distance))/2)
+	low := math.Floor((time - math.Sqrt(math.Pow(time, 2) - 4 * distance))/2)
+	high := math.Ceil((time + math.Sqrt(math.Pow(time, 2) - 4 * distance))/2)
 
 	return int(high - low - 1)
 }
