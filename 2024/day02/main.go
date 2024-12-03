78cc3b2cec7b90727d397ebd2315bb62afd3caf6
andrewelkins <github2@andrewelkins.com>
2024-12-03 10:10:51 -0800

    Day 2

--- 2024/day02/main.go
+++ 2024/day02/main.go
@@ -41,12 +41,89 @@ func main() {
 func part1(input string) int {
 	parsed := parseInput(input)
 	_ = parsed
+	var count int
 
-	return 0
+	for _, line := range parsed {
+		parts := strings.Split(line, " ")
+		var abort bool = false
+		var previous int = 0
+		var ascending bool = true
+		var absDif int
+
+		for k, part := range parts {
+			if k == 0 {
+				previous = cast.ToInt(part)
+			} else {
+				if k == 1 {
+					if cast.ToInt(part) < previous {
+						ascending = false
+					}
+				}
+				if ascending {
+					absDif = cast.ToInt(part) - previous
+				} else {
+					absDif = previous - cast.ToInt(part)
+				}
+
+				if !abort && absDif > 0 && absDif < 4 {
+					previous = cast.ToInt(part)
+				} else {
+					abort = true
+				}
+			}
+		}
+		if !abort {
+			count += 1
+		}
+	}
+
+	return count
 }
 
 func part2(input string) int {
-	return 0
+	parsed := parseInput(input)
+	_ = parsed
+	var count int = 3
+
+	for _, line := range parsed {
+		parts := strings.Split(line, " ")
+		var abort bool = false
+		var previous int = 0
+		var ascending bool = true
+		var absDif int
+		var abortSafety int = 0
+
+		for k, part := range parts {
+			if k == 0 {
+				previous = cast.ToInt(part)
+			} else {
+				if k == 1 || (k == 2 && abortSafety == 1) {
+					if cast.ToInt(part) < previous {
+						ascending = false
+					}
+				}
+				if ascending {
+					absDif = cast.ToInt(part) - previous
+				} else {
+					absDif = previous - cast.ToInt(part)
+				}
+
+				if !abort && absDif > 0 && absDif < 4 {
+					previous = cast.ToInt(part)
+				} else {
+					abortSafety += 1
+					if abortSafety > 1 && k == len(parts) - 1 {
+						abort = true
+					}
+				}
+			}
+		}
+		if !abort {
+			count += 1
+		}
+	}
+
+	return count
 }
 
 func parseInput(input string) (ans []string) {
@@ -55,3 +132,11 @@ func parseInput(input string) (ans []string) {
 	}
 	return ans
 }
+
+
+func absDiffInt(x, y int) int {
+	if x < y {
+	   return y - x
+	}
+	return x - y
+ }
