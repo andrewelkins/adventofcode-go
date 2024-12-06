6095c512f3d50d4a6c10b5d030c4ffb5b7c80e6c
andrewelkins <github2@andrewelkins.com>
2024-12-06 12:29:11 -0800

    Day6

--- 2024/day06/main.go
+++ 2024/day06/main.go
@@ -5,8 +5,8 @@ import (
 	"flag"
 	"fmt"
 	"strings"
+	"time"
 
-	"github.com/andrewelkins/adventofcode-go/cast"
 	"github.com/andrewelkins/adventofcode-go/util"
 )
 
@@ -39,13 +39,129 @@ func main() {
 }
 
 func part1(input string) int {
+
+	start := time.Now()
+
 	parsed := parseInput(input)
 	_ = parsed
+	xcount := 0
+	var do bool = true
+	lab := make(map[int][]string)
+	cursor := [2]int{0, 0}
 
-	return 0
+	for i, line := range parsed {
+		lab[i] = strings.Split(line, "")
+		start := indexOf(lab[i], "^") // ^ > v <  u l r d
+		if start != -1 && cursor[0] == 0 {
+			cursor = [2]int{i, start}
+		}
+	}
+
+	for do {
+		if !cursorCheck(lab, cursor) {
+			do = false
+			break
+		}
+		switch lab[cursor[0]][cursor[1]] {
+		case "^":
+			lab[cursor[0]][cursor[1]] = "X"
+			if lab[cursor[0]-1][cursor[1]] == "#" {
+				cursor[1]++
+				if !cursorCheck(lab, cursor) {
+					do = false
+				} else {
+					lab[cursor[0]][cursor[1]] = ">"
+				}
+			} else {
+				cursor[0]--
+				if !cursorCheck(lab, cursor) {
+					do = false
+				} else {
+					lab[cursor[0]][cursor[1]] = "^"
+				}
+			}
+		case ">":
+			lab[cursor[0]][cursor[1]] = "X"
+			if lab[cursor[0]][cursor[1]+1] == "#" {
+				cursor[0]++
+				if !cursorCheck(lab, cursor) {
+					do = false
+				} else {
+					lab[cursor[0]][cursor[1]] = "v"
+				}
+			} else {
+				cursor[1]++
+				if !cursorCheck(lab, cursor) {
+					do = false
+				} else {
+					lab[cursor[0]][cursor[1]] = ">"
+				}
+			}
+		case "v":
+			lab[cursor[0]][cursor[1]] = "X"
+			if cursor[0]+1 >= len(lab[cursor[0]]) {
+				do = false
+				break
+			}
+			if lab[cursor[0]+1][cursor[1]] == "#" {
+				cursor[1]--
+				if !cursorCheck(lab, cursor) {
+					do = false
+				} else {
+					lab[cursor[0]][cursor[1]] = "<"
+				}
+			} else {
+				cursor[0]++
+				if !cursorCheck(lab, cursor) {
+					do = false
+				} else {
+					lab[cursor[0]][cursor[1]] = "v"
+				}
+			}
+		case "<":
+			lab[cursor[0]][cursor[1]] = "X"
+			if lab[cursor[0]][cursor[1]-1] == "#" {
+				cursor[0]--
+				if !cursorCheck(lab, cursor) {
+					do = false
+				} else {
+					lab[cursor[0]][cursor[1]] = "^"
+				}
+			} else {
+				cursor[1]--
+				if !cursorCheck(lab, cursor) {
+					do = false
+				} else {
+					lab[cursor[0]][cursor[1]] = "<"
+				}
+			}
+		default:
+			do = false
+		}
+		// // fmt.Print("\033[2J") // clear the screen
+		// fmt.Print("====================================\n")
+		// for i, _ := range lab[0] {
+		// 	fmt.Println(lab[i])
+		// }
+
+	}
+
+	for i, _ := range lab[0] {
+		for _, char := range lab[i] {
+			if char == "X" {
+				xcount++
+			}
+		}
+	}
+
+	elapsed := time.Since(start)
+	fmt.Printf("page took %s \n", elapsed)
+
+	return xcount
 }
 
 func part2(input string) int {
+
 	return 0
 }
 
@@ -54,4 +170,20 @@ func parseInput(input string) (ans []string) {
 		ans = append(ans, line)
 	}
 	return ans
+}
+
+func indexOf(haystack []string, needle string) int {
+	for i, v := range haystack {
+		if v == needle {
+			return i
+		}
+	}
+	return -1
+}
+
+func cursorCheck(lab map[int][]string, cursor [2]int) bool {
+	if cursor[0] < 0 || cursor[0] > len(lab) || cursor[1] < 0 || cursor[1] > len(lab[cursor[0]]) {
+		return false
+	}
+	return true
 }
